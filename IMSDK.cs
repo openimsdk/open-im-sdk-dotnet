0c8a5b51238cf9d22833a278a050720c6614d50b
yejian <596306006@qq.com>
2024-03-21 21:39:24 +0800

    fix: args error

--- IMSDK.cs
+++ IMSDK.cs
@@ -534,17 +534,17 @@ namespace open_im_sdk
             callBackDic[operationID] = cb;
             IMNativeSDK.delete_conversation_and_delete_all_msg(operationID, conversationID);
         }
-        public static void InsertSingleMessageToLocalStorage(OnMsgStruct cb, string message, string recvID, string sendID)
+        public static void InsertSingleMessageToLocalStorage(OnMsgStruct cb, MsgStruct message, string recvID, string sendID)
         {
             var operationID = GetOperationID(System.Reflection.MethodBase.GetCurrentMethod().Name);
             callBackDic[operationID] = cb;
-            IMNativeSDK.insert_single_message_to_local_storage(operationID, message, recvID, sendID);
+            IMNativeSDK.insert_single_message_to_local_storage(operationID, Utils.ToJson(message), recvID, sendID);
         }
-        public static void InsertGroupMessageToLocalStorage(OnMsgStruct cb, string message, string groupID, string sendID)
+        public static void InsertGroupMessageToLocalStorage(OnMsgStruct cb, MsgStruct message, string groupID, string sendID)
         {
             var operationID = GetOperationID(System.Reflection.MethodBase.GetCurrentMethod().Name);
             callBackDic[operationID] = cb;
-            IMNativeSDK.insert_group_message_to_local_storage(operationID, message, groupID, sendID);
+            IMNativeSDK.insert_group_message_to_local_storage(operationID, Utils.ToJson(message), groupID, sendID);
         }
         public static void SearchLocalMessages(OnSearchLocalMessagesCallback cb, SearchLocalMessagesParams searchParam)
         {
